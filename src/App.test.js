fca60e1af1e1a86b24d82dabe93209e32529504d
Medini Ghazi <58168231+MediniGhazi0896@users.noreply.github.com>
2024-01-29 15:31:57 +0100

    This Repo is a challenge made with FEV.IO company as a technical test in order to gain an internship with them!
    Added The Header Menu + DropDown

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+/* import { DropDown } from './screens/DropDown/DropDown';
+import { Menu } from './screens/Menu';
+ */
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
