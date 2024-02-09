5b945d186975e3de554ba39cfb9fe5b8c37ae48a
Medini Ghazi <58168231+MediniGhazi0896@users.noreply.github.com>
2024-02-09 13:10:46 +0100

    Added Static Chat Widget

--- src/screens/Section/Section.jsx
+++ src/screens/Section/Section.jsx
@@ -8,22 +8,20 @@ export const Section = () => {
     <div className="section">
       <div className="div">
         <div className="overlap">
-          <div class="container">
-            <div class="left">
+          <div className="container">
+            <div className="left">
             </div>
-            <div class="right">
+            <div className="right">
+              <div className="right1"></div>
+              <div className="right2"></div>
+              <div className="right3"></div>
+            </div>
+          </div>
           
-            <div class="right1"></div>
-              <div class="right2"></div>
-              <div class="right3"></div>
-            </div>
-          </div>
-          <div class="road">
-          <img className="Car" alt="Group" src="https://c.animaapp.com/wBFcRtRx/img/group-427320261.png" />
-          </div>
-          <Chat/> {/* Here Is A call For The Chat Component */}
+            <img className="Car" alt="Group" src="https://c.animaapp.com/wBFcRtRx/img/group-427320261.png" />
+          <Chat /> {/* Here Is A call For The Chat Component */}
         </div>
-        <SectionLeftHalf/>
+        <SectionLeftHalf /> {/* Here Is A call For The Section's LeftHalf Component */}
       </div>
     </div>
   );
