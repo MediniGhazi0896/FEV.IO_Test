6da328ec3be3bbdb1f25cffca137b58f4b325de9
Medini Ghazi <58168231+MediniGhazi0896@users.noreply.github.com>
2024-02-09 15:35:07 +0100

    Changed Navbar's General Look

--- src/screens/Menu/Menu.jsx
+++ src/screens/Menu/Menu.jsx
@@ -5,14 +5,11 @@ export const Menu = () => {
   return (
     <div className="menu">
       <header className="navbar-wrapper">
-        <div className="text-wrapper">solutions</div>
-        <div className="text-wrapper-2">who we are</div>
+      <div className="text-wrapper">who we are</div>
+        <div className="text-wrapper-2">solutions</div>
         <div className="text-wrapper-3">technology</div>
         <div className="text-wrapper-4">careers</div>
-        <div className="group">
-          <div className="text-wrapper-5">blog</div>
-        </div>
-
+        <div className="text-wrapper-5">blog</div>
         <div className="group-wrapper">
           <div className="div">
             <img className="img" alt="Group" src="https://c.animaapp.com/KAlTT52j/img/group-210@2x.png" />
